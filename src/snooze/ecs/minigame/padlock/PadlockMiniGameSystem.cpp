4e6a0dfdb57f89014e25b86f035085cb13237271
Spiderboy <contact@spiderboy.fr>
2022-04-02 23:12:44 +0200

    Add simple lock/unlock mechanism

--- src/snooze/ecs/minigame/padlock/PadlockMiniGameSystem.cpp
+++ src/snooze/ecs/minigame/padlock/PadlockMiniGameSystem.cpp
@@ -3,6 +3,19 @@
 
 #include <iostream>
 
+#include <forge/engine/data/api/DataAPI.h>
+#include <forge/engine/ecs/Entity.h>
+
+#include <snooze/data/DataList.h>
+#include <snooze/data/EntityCatalog.h>
+
+//----------------------------------------------------------------------------
+PadlockMiniGameSystem::PadlockMiniGameSystem()
+    : m_KeyAcquired(false)
+    , m_PadlockUnlocked(false)
+{
+}
+
 //----------------------------------------------------------------------------
 void PadlockMiniGameSystem::Execute(const u64& _dt, const forge::Entity::Ptr& _entity)
 {
@@ -10,8 +23,16 @@ void PadlockMiniGameSystem::Execute(const u64& _dt, const forge::Entity::Ptr& _e
 
     BaseMiniGame::Update(comp);
 
-    if (m_Chrono.IsElapsed())
+    if (m_KeyAcquired && m_Key != nullptr)
     {
+        RequestRemoveEntity(m_Key);
+        m_Key = nullptr;
+    }
+    if (m_PadlockUnlocked)
+    {
+        RequestRemoveEntity(m_Lock);
+        m_Lock = nullptr;
+
         BaseMiniGame::CompleteGame(comp);
     }
 }
@@ -19,13 +40,46 @@ void PadlockMiniGameSystem::Execute(const u64& _dt, const forge::Entity::Ptr& _e
 //----------------------------------------------------------------------------
 void PadlockMiniGameSystem::OnMiniGameStart()
 {
-    std::cout << "PadlockMiniGameSystem starts" << std::endl;
+    forge::builtin::EntityClickedEvent::Handlers +=
+        forge::builtin::EntityClickedEvent::Handler(this, &PadlockMiniGameSystem::OnEntityClickedEvent);
 
-    m_Chrono.Start(5000);
+    m_Lock = forge::DataAPI::GetDataFrom<EntityCatalog>(DataList::Entity::LockLock);
+    m_Lock->SetPosition(57, 57, 0);
+    RequestAddEntity(m_Lock);
+/*
+forge::Entity::Ptr screw1 = forge::DataAPI::GetDataFrom<EntityCatalog>(DataList::Entity::LockChainBack);
+    screw1->SetPosition(50.f - screw1->GetSize().w * 0.5f, 50.f - screw1->GetSize().d * 0.5f, 0.f);
+    m_World.AddEntity(screw1);
+
+    forge::Entity::Ptr screw = forge::DataAPI::GetDataFrom<EntityCatalog>(DataList::Entity::LockChainFront);
+    screw->SetPosition(50.f - screw->GetSize().w * 0.5f, 50.f - screw->GetSize().d * 0.5f, 1.f);
+    m_World.AddEntity(screw);
+
+    forge::Entity::Ptr button = forge::DataAPI::GetDataFrom<EntityCatalog>(DataList::Entity::LockLock);
+    button->SetPosition(50.f - button->GetSize().w * 0.5f, 50.f - button->GetSize().d * 0.5f, 1.f);
+    m_World.AddEntity(button);
+*/
 }
 
 //----------------------------------------------------------------------------
 void PadlockMiniGameSystem::OnMiniGameStop()
 {
-    std::cout << "PadlockMiniGameSystem stops" << std::endl;
+    forge::builtin::EntityClickedEvent::Handlers -=
+        forge::builtin::EntityClickedEvent::Handler(this, &PadlockMiniGameSystem::OnEntityClickedEvent);
+}
+
+//----------------------------------------------------------------------------
+void PadlockMiniGameSystem::OnEntityClickedEvent(const forge::builtin::EntityClickedEvent& _event)
+{
+    if (_event.GetIsPressed())
+        return;
+
+    if (_event.GetEntity() == m_Key)
+    {
+        m_KeyAcquired = true;
+    }
+    else if (_event.GetEntity() == m_Lock && m_KeyAcquired)
+    {
+        m_PadlockUnlocked = true;
+    }
 }
--- src/snooze/ecs/minigame/padlock/PadlockMiniGameSystem.h
+++ src/snooze/ecs/minigame/padlock/PadlockMiniGameSystem.h
@@ -3,6 +3,8 @@
 #include <forge/engine/ecs/System.h>
 #include <forge/engine/time/Chrono.h>
 
+#include <forge/builtin/3Cs/events/MouseEvent.h>
+
 #include <snooze/ecs/minigame/BaseMiniGame.h>
 #include <snooze/ecs/minigame/padlock/PadlockMiniGameComponent.h>
 
@@ -11,13 +13,21 @@ class PadlockMiniGameSystem : public BaseMiniGame
                             , public forge::System<PadlockMiniGameComponent>
 {
 public:
+    PadlockMiniGameSystem();
+
     void Execute(const u64& _dt, const forge::Entity::Ptr& _entity) override;
 
 private:
     void OnMiniGameStart() override;
     void OnMiniGameStop() override;
 
+    void OnEntityClickedEvent(const forge::builtin::EntityClickedEvent& _event);
+
 private:
-    forge::Chrono   m_Chrono;
+    forge::Entity::Ptr m_Key;
+    forge::Entity::Ptr m_Lock;
+
+    bool    m_KeyAcquired;
+    bool    m_PadlockUnlocked;
 
 };
