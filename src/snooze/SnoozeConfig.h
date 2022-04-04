e09a38c31b8c2b5c8eb7546db1b72233d67723c9
Roulyo <roulyo.42@gmail.com>
2022-04-04 13:44:15 +0200

    adds static spawn points

--- src/snooze/SnoozeConfig.h
+++ src/snooze/SnoozeConfig.h
@@ -7,4 +7,21 @@ struct SnoozeConfig
     static constexpr f32 MiniGameTimeRatioToSpawn = 0.1f;
     static constexpr f32 SpeechCharactersPerSecond = 42.0f;
 
+    static constexpr u32 ToolSpawnPointCount = 6;
+    static constexpr forge::Vector3f ToolSpawnPoints[ToolSpawnPointCount] =
+    {
+        { 40.f, 40.f, 10.f },
+        { 56.f, 56.f, 10.f },
+        { 40.f, 56.f, 10.f },
+        { 56.f, 40.f, 10.f },
+        { 41.f, 50.f,  1.f },
+        { 54.f, 49.f,  1.f }
+    };
+
+    static constexpr u32 MobSpawnPointCount = 1;
+    static constexpr forge::Vector3f MobSpawnPoints[MobSpawnPointCount] =
+    {
+        // TODO: fill mob spawn points
+        { 40.f, 40.f, 10.f }
+    };
 };
